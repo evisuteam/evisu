1ef7a516ba26de3bdd7644e2ee727166adbc5332
grimnessljr <1352197989@qq.com>
2019-11-09 10:31:33 +0800

    no message

--- manage/model/DB.php
+++ manage/model/DB.php
@@ -11,11 +11,6 @@ class DB{
     private $username = 'dage';
     private $password = '123456';
     private $database = 'evisu';
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 97756ac0169b1411a486292497c57796e9f3a2c8
     public $mysqli;//数据库链接 $con
 
     function __construct()
